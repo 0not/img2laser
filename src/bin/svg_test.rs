59db43832955e6115becff51c29125915bf58213
Kyle Larsen <larsenkg@gmail.com>
2023-08-16 16:49:11 -0600

    It works!
    Next steps include breaking out code into separate files, wasm support,
    and making all parameters configurable.

--- src/bin/svg_test.rs
+++ src/bin/svg_test.rs
@@ -2,36 +2,59 @@ use svg::node::element::path::{Command, Data, Position};
 use svg::node::element::Path;
 use svg::Document;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let width = 100;
-    let height = 100;
-    let fs: f32 = 10.; // spatial sample frequency
-    let start_x = 0.;
-    let start_y = 2.;
-    let amp = 1.8;
+use std::io;
 
+struct SvgConfig {
+    width: usize,
+    height: usize,
+    fs: f32, // spatial sample frequency
+    start_x: f32,
+    start_y: f32,
+    amp: f32,
+}
+
+impl Default for SvgConfig {
+    fn default() -> Self {
+        SvgConfig {
+            width: 100,
+            height: 100,
+            fs: 10., // spatial sample frequency
+            start_x: 0.,
+            start_y: 2.,
+            amp: 1.8,
+        }
+    }
+}
+
+fn save_svg(c: &SvgConfig) -> io::Result<()> {
     // Create sine wave
-    let sine1 = (0..width * (fs as i32))
+    let sine1 = (0..c.width * (c.fs as usize))
         .flat_map(|xi| {
-            let x = start_x + (xi as f32) / fs;
-            let y = start_y + amp * (x).sin();
+            let x = c.start_x + (xi as f32) / c.fs;
+            let y = c.start_y + c.amp * (x).sin();
             vec![x, y]
         })
         .collect::<Vec<f32>>();
 
-    let sine2 = (0..width * (fs as i32))
+    let sine2 = (0..c.width * (c.fs as usize))
         .flat_map(|xi| {
-            let x = start_x + (xi as f32) / fs;
-            let y = 6. + amp * x.sin();
+            let x = c.start_x + (xi as f32) / c.fs;
+            let y = 6. + c.amp * x.sin();
             vec![x, y]
         })
         .collect::<Vec<f32>>();
 
-    let data = Data::new()
-        .move_to((0, 2))
-        .add(Command::Line(Position::Absolute, sine1.into()))
-        .move_to((0, 6))
-        .add(Command::Line(Position::Absolute, sine2.into()));
+    // let data = Data::new()
+    //     .move_to((0, 2))
+    //     .add(Command::Line(Position::Absolute, sine1.into()))
+    //     .move_to((0, 6))
+    //     .add(Command::Line(Position::Absolute, sine2.into()));
+
+    let mut data = Data::new();
+    data = data.move_to((0, 2));
+    data = data.add(Command::Line(Position::Absolute, sine1.into()));
+    data = data.move_to((0, 6));
+    data = data.add(Command::Line(Position::Absolute, sine2.into()));
 
     let path = Path::new()
         .set("fill", "none")
@@ -40,7 +63,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set("d", data);
 
     let document = Document::new()
-        .set("viewBox", (0, 0, width, height))
+        .set("viewBox", (0, 0, c.width, c.height))
         .add(path);
 
     svg::save("examples/image.svg", &document)?;
@@ -48,28 +71,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// use plotters::prelude::*;
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let drawing_area = SVGBackend::new("examples/chart_builder_on.svg", (300, 200))
-//         .into_drawing_area();
-
-//     drawing_area.draw(&PathElement::new(
-//         (0..500).map(|x| (x / 10, 10 + (10.*((x / 10) as f32).sin()) as i32)).collect::<Vec<_>>()
-//         , &BLACK))?;
-//     // chart
-//         // .draw_series(LineSeries::new(
-//         //     (-500..=500).map(|x| x as f32 / 500.0).map(|x| (x, x * x)),
-//         //     &RED,
-//         // ))?
-//         // .draw_series(std::iter::once(&PathElement::new(vec![(-1, 0), (1, 0)], &RED)));
-//         // .label("y = x^2")
-//         // .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-//     // chart_context
-//     //     .configure_mesh()
-//     //     .draw()
-//     //     .unwrap();
-
-//     Ok(())
-// }
+fn main() -> io::Result<()> {
+    let svg_config = SvgConfig::default();
+    save_svg(&svg_config)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use image::{self, DynamicImage, GenericImageView, ImageBuffer};
+use image::{DynamicImage, GenericImageView};
+
+use ndarray::{s, Array1, Array2, Axis};
+use nshare::ToNdarray2;
+
+use svg::node::element::path::{Command, Data, Position};
+use svg::node::element::Path;
+use svg::Document;
 
 /// Default values
-const LINES: u32 = 32;
+const LINES: usize = 64;
 
 /// A program that takes in a bitmap image and outputs a line shaded SVG
 #[derive(Parser)]
@@ -18,94 +25,223 @@ struct Cli {
 
     /// Number of lines
     #[arg(short, long, default_value_t = LINES)]
-    lines: u32,
+    lines: usize,
 }
 
 struct LineShadingConfig {
-    lines: u32,
+    lines: usize,
+    svg_config: SvgConfig,
 }
 
 impl Default for LineShadingConfig {
     fn default() -> Self {
-        LineShadingConfig { lines: LINES }
+        LineShadingConfig {
+            lines: LINES,
+            svg_config: SvgConfig::default(),
+        }
     }
+}
+
+struct SvgConfig {
+    width: usize,
+    height: usize,
+    fs: f32, // spatial sample frequency
+    amp: f32,
+}
+
+impl Default for SvgConfig {
+    fn default() -> Self {
+        SvgConfig {
+            width: 512,
+            height: 512,
+            fs: 10., // spatial sample frequency
+            amp: 0.4,
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ImageProcessError {
+    #[error(transparent)]
+    ImageError(#[from] image::ImageError),
+
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    // #[error("failed to perform 'mean'")]
+    // NdarrayMeanError(),
 }
 
 fn process_image<P>(
     in_path: P,
     out_path: P,
     config: &LineShadingConfig,
-) -> image::ImageResult<image::DynamicImage>
+) -> Result<(), ImageProcessError>
 where
     P: AsRef<std::path::Path>,
 {
+    // Spatial sampling frequency
+    let fs = config.svg_config.fs;
+    let width = config.svg_config.width;
+    let height = config.svg_config.height;
+    let row_height = height as f32 / config.lines as f32;
+    let amp = config.svg_config.amp * row_height;
+
     // Open image
     let img = image::open(in_path)?;
 
-    // Convert to grayscale and average the rows
-    let out = average_rows(&img, config);
+    // Average the rows
+    let avgs = average_rows(&img, config);
+    let lines = make_lines(&avgs);
+    // let (rows, samples) = lines.dim();
 
-    out.save(out_path)?;
+    let mut data = Data::new();
+    for (yi, row) in lines.axis_iter(Axis(0)).enumerate() {
+        // TODO: Everything should be shifted down by half of a row so that the
+        // first row is not cut off. Will need to address the bottom too.
+        // I think row_height will need to be adjust to accout for the amplitude
+        // of the sine wave.
+        let y_offset = yi as f32 * row_height;
 
-    return Ok(out);
-}
+        let sine = row
+            .iter()
+            .enumerate()
+            .flat_map(|(xi, &y)| {
+                let x = xi as f32 / fs;
 
-fn average_rows(img: &DynamicImage, config: &LineShadingConfig) -> DynamicImage {
-    // Convert to grayscale. Using `grayscale()` was leaving the image in Rgba8,
-    // which means we'd have to deal with 3 channels.
-    let img = img.clone().into_luma8(); // TODO: Is clone really needed?
+                vec![x, amp * y + y_offset]
+            })
+            .collect::<Vec<f32>>();
 
-    // Grab the image dimensions and force config.lines <= height
-    let (width, height) = img.dimensions();
-    let lines = if config.lines <= height {
-        config.lines
-    } else {
-        height
-    };
+        let x = sine[0];
+        let y = sine[1];
 
-    // Create an empty image buffer to store the averaged rows.
-    let mut out = ImageBuffer::new(width, height);
-
-    // Iterate over the rows
-    let mut y = 0;
-    let mut n_rows = 0;
-    while y < (height - 1) {
-        // Vertical height of current row
-        let row_h = (height - y) / (lines - n_rows);
-        n_rows += 1;
-
-        // Iterate over the columns in each row
-        for x in 0..width {
-            // Grab a 1 pixel wide vertical slice of each row
-            let row = img.view(x, y, 1, row_h);
-            let (w, h) = row.dimensions();
-
-            // Average the vertical slice
-            let avg = row
-                .pixels()
-                .map(|(_, _, pixel)| pixel.0[0] as u32)
-                .sum::<u32>()
-                / (w * h);
-            let avg: u8 = avg.try_into().unwrap();
-
-            // Write the average value to the pixels that were averaged
-            for yd in 0..row_h {
-                out.put_pixel(x, y + yd, image::Luma([avg]));
-            }
-        }
-        y += row_h;
+        data = data.move_to((x, y));
+        data = data.add(Command::Line(Position::Absolute, sine.into()))
     }
 
-    return DynamicImage::ImageLuma8(out);
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 0.1)
+        .set("d", data);
+
+    let document = Document::new()
+        .set("viewBox", (0, 0, width, height))
+        .add(path);
+
+    svg::save(out_path, &document)?;
+
+    // println!("{:?}", lines);
+
+    // println!("{:?}", avgs);
+
+    // // Resize averaged image to original size
+    // let new_img = GrayImage::from_raw(512, config.lines as u32, avgs.into_raw_vec()).unwrap();
+    // let new_img = image::imageops::resize(&new_img, 512, 512, image::imageops::FilterType::Nearest);
+    // new_img.save("examples/gray_2.png")?;
+
+    Ok(())
+}
+
+// TODO: Add support for transparency (locations where no line will be drawn)
+fn average_rows(img: &DynamicImage, config: &LineShadingConfig) -> Array2<u8> {
+    // Grab the image dimensions and force config.lines <= height
+    let (width, height) = img.dimensions();
+
+    let lines = if config.lines <= height as usize {
+        config.lines
+    } else {
+        height as usize
+    };
+
+    let row_height = height as usize / lines;
+    let mut result = Array2::zeros((lines, width as usize));
+
+    // Must cast to u32 or 'mean' operation will overflow.
+    let img_array = img.to_luma8().into_ndarray2().mapv(|e| u32::from(e));
+
+    for n in 0..lines {
+        let start = n * row_height;
+        let end = (n + 1) * row_height;
+        let row: Array1<u32> = img_array
+            .slice(s![start..end, ..])
+            .mean_axis(Axis(0))
+            .unwrap(); // TODO: Handle safely?
+
+        result.slice_mut(s![n, ..]).assign(&row);
+    }
+
+    // This cast shouldn't be lossy because pre-averaged values were u8
+    result.mapv(|e| e as u8)
+}
+
+fn make_lines(img: &Array2<u8>) -> Array2<f32> {
+    // Spatial "sampling frequency". If lower, the processing
+    //  will be faster, but at the sake of poorer spatial resolution
+    //  (sine waves won't look like sine waves)
+    let fs: f32 = 10.;
+
+    // The spatial frequency will be scaled to be within these bounds
+    let f_min_new: f32 = 0.001;
+    let f_max_new: f32 = 0.5;
+
+    let (rows, cols) = img.dim();
+    // let lines = Array2::zeros((rows, cols));
+
+    // Horizontal sample locations
+    let x = Array1::range(0., cols as f32, 1. / fs);
+
+    // The phase of the sine waves
+    let mut phi = Array2::zeros((rows, x.len()));
+
+    // The frequencies come from the image pixel values (intensity)
+    // let frequencies = (u8::MAX - img) / u8::MAX;
+    let frequencies = img.mapv(|x| f32::from(u8::MAX - x) / f32::from(u8::MAX));
+
+    // Global min. frequency
+    let f_min = frequencies.iter().copied().reduce(f32::min).unwrap();
+
+    // Global max. frequency
+    let f_max = frequencies.iter().copied().reduce(f32::max).unwrap();
+
+    for r in 0..rows {
+        // Linearly scale the frequencies in to the new range.
+        let f_slice = frequencies.slice(s![r, ..]);
+        let scale = if f_max - f_min != 0. {
+            (f_max_new - f_min_new) / (f_max - f_min)
+        } else {
+            1.
+        };
+        let freqs: Array1<f32> = f_slice.mapv(|f| f_min_new + scale * (f - f_min));
+
+        // Initialize the frequency array to zeros
+        let mut f = Array1::<f32>::zeros(x.len());
+
+        // Loop through the f array and sample the value from the freqs array
+        for n in 0..f.len() {
+            let i = (n as f32 / fs).floor() as usize;
+            f[n] = freqs[i];
+        }
+
+        f.accumulate_axis_inplace(Axis(0), |&prev, curr| *curr += prev);
+        //f /= fs;
+        phi.slice_mut(s![r, ..]).assign(&f);
+    }
+
+    // Return the sine waves
+    phi.mapv(|x| x.sin())
 }
 
 fn main() {
     let cli = Cli::parse();
 
     let mut out_path = cli.input.clone();
-    out_path.set_extension("jpg");
+    out_path.set_extension("svg");
 
-    let config = LineShadingConfig { lines: cli.lines };
+    let config = LineShadingConfig {
+        lines: cli.lines,
+        ..Default::default()
+    };
 
     if let Ok(_) = process_image(cli.input.as_path(), out_path.as_path(), &config) {
         println!("Successfully saved image.");
